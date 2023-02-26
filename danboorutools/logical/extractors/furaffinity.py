40e218c13226f2d745197aed7353c9646037d2d2
nonamethanks <hellafrickingepic@gmail.com>
2023-02-26 18:34:57 +0100

    Normalize furaffinity urls

--- danboorutools/logical/extractors/furaffinity.py
+++ danboorutools/logical/extractors/furaffinity.py
@@ -8,15 +8,32 @@ class FuraffinityUrl(Url):
 class FuraffinityPostUrl(PostUrl, FuraffinityUrl):
     post_id: int
 
+    @classmethod
+    def normalize(cls, **kwargs) -> str | None:
+        return f"https://www.furaffinity.net/view/{kwargs['post_id']}"
+
 
 class FuraffinityArtistUrl(ArtistUrl, FuraffinityUrl):
     username: str
+
+    @classmethod
+    def normalize(cls, **kwargs) -> str | None:
+        return f"https://www.furaffinity.net/user/{kwargs['username']}"
 
 
 class FuraffinityImageUrl(PostAssetUrl, FuraffinityUrl):
     username: str | None
     post_id: int | None
 
+    @property
+    def full_size(self) -> str:
+        if self.parsed_url.subdomain == "d":
+            return self.parsed_url.raw_url
+        else:
+            raise NotImplementedError
+
 
 class FuraffinityArtistImageUrl(GalleryAssetUrl, FuraffinityUrl):
-    ...
+    @property
+    def full_size(self) -> str:
+        return self.parsed_url.raw_url
