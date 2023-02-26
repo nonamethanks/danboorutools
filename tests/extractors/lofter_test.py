28664a527952383f95d2895945dcaea3bec7ee00
nonamethanks <hellafrickingepic@gmail.com>
2023-02-26 18:52:52 +0100

    Add lofter normalization tests

---
+++ tests/extractors/lofter_test.py
@@ -0,0 +1,25 @@
+from ward import test
+
+from danboorutools.models.url import Url
+
+urls = {
+    "https://imglf3.lf127.net/img/S1d2QlVsWkJhSW1qcnpIS0ZSa3ZJSzFCWFlnUWgzb01DcUdpT1lreG5yQjJVMkhGS09HNGR3PT0.png?imageView&thumbnail=1680x0&quality=96&stripmeta=0": "https://imglf3.lf127.net/img/S1d2QlVsWkJhSW1qcnpIS0ZSa3ZJSzFCWFlnUWgzb01DcUdpT1lreG5yQjJVMkhGS09HNGR3PT0.png",
+    "https://imglf3.lf127.net/img/S1d2QlVsWkJhSW1qcnpIS0ZSa3ZJSzFCWFlnUWgzb01DcUdpT1lreG5yQjJVMkhGS09HNGR3PT0.png": "https://imglf3.lf127.net/img/S1d2QlVsWkJhSW1qcnpIS0ZSa3ZJSzFCWFlnUWgzb01DcUdpT1lreG5yQjJVMkhGS09HNGR3PT0.png",
+    "http://imglf0.nosdn.127.net/img/cHl3bXNZdDRaaHBnNWJuN1Y4OXBqR01CeVBZSVNmU2FWZWtHc1h4ZTZiUGxlRzMwZnFDM1JnPT0.jpg": "http://imglf0.nosdn.127.net/img/cHl3bXNZdDRaaHBnNWJuN1Y4OXBqR01CeVBZSVNmU2FWZWtHc1h4ZTZiUGxlRzMwZnFDM1JnPT0.jpg",
+
+    "https://www.lofter.com/front/blog/home-page/noshiqian": "https://noshiqian.lofter.com",
+    "http://www.lofter.com/app/xiaokonggedmx": "https://xiaokonggedmx.lofter.com",
+    "http://www.lofter.com/blog/semblance": "https://semblance.lofter.com",
+    "http://gengar563.lofter.com": "https://gengar563.lofter.com",
+
+    "https://gengar563.lofter.com/post/1e82da8c_1c98dae1b": "https://gengar563.lofter.com/post/1e82da8c_1c98dae1b",
+}
+
+
+for original_string, normalized_string in urls.items():
+    parsed_url = Url.parse(original_string)
+    domain = parsed_url.parsed_url.domain
+
+    @test(f"Normalizing {domain}: {original_string}", tags=["parsing", "normalization", domain])
+    def _(_parsed_url=parsed_url, _normalized_string=normalized_string) -> None:
+        assert _parsed_url.normalized_url == _normalized_string
