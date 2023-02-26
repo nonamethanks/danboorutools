b63918f84d8fdee02d4be9acfa01a7887daf4a8a
nonamethanks <hellafrickingepic@gmail.com>
2023-02-26 18:50:09 +0100

    Normalize instagram urls

--- danboorutools/logical/extractors/instagram.py
+++ danboorutools/logical/extractors/instagram.py
@@ -8,6 +8,14 @@ class InstagramUrl(Url):
 class InstagramPostUrl(PostUrl, InstagramUrl):
     post_id: str
 
+    @classmethod
+    def normalize(cls, **kwargs) -> str | None:
+        return f"https://www.instagram.com/p/{kwargs['post_id']}"
+
 
 class InstagramArtistUrl(ArtistUrl, InstagramUrl):
     username: str
+
+    @classmethod
+    def normalize(cls, **kwargs) -> str | None:
+        return f"https://www.instagram.com/{kwargs['username']}"
