89541e7680b3b011e04cccda352068820d03b28c
nonamethanks <hellafrickingepic@gmail.com>
2023-03-01 14:22:38 +0100

    Normalize plurk urls

--- danboorutools/logical/extractors/plurk.py
+++ danboorutools/logical/extractors/plurk.py
@@ -8,10 +8,18 @@ class PlurkUrl(Url):
 class PlurkPostUrl(PostUrl, PlurkUrl):
     post_id: str
 
+    normalize_string = "https://www.plurk.com/p/{post_id}"
+
 
 class PlurkArtistUrl(ArtistUrl, PlurkUrl):
     username: str
 
+    normalize_string = "https://www.plurk.com/{username}"
+
 
 class PlurkImageUrl(PostAssetUrl, PlurkUrl):
     image_id: str
+
+    @property
+    def full_size(self) -> str:
+        return f"https://images.plurk.com/{self.image_id}.{self.parsed_url.extension}"
