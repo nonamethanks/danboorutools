1ed82cbe8cca8904f8aefdb59b2584ba6117b501
nonamethanks <hellafrickingepic@gmail.com>
2023-03-09 10:36:38 +0100

    Fix plurk tests

--- danboorutools/logical/parsers/plurk.py
+++ danboorutools/logical/parsers/plurk.py
@@ -3,45 +3,41 @@ from danboorutools.logical.parsers import ParsableUrl, UrlParser
 
 
 class PlurkComParser(UrlParser):
-    test_cases = {
-        PlurkArtistUrl: [
-            "https://www.plurk.com/m/redeyehare",
-            "https://www.plurk.com/u/ddks2923",
-            "https://www.plurk.com/m/u/leiy1225",
-            "https://www.plurk.com/s/u/salmonroe13",
-            "https://www.plurk.com/redeyehare",
-            "https://www.plurk.com/RSSSww/invite/4",
-        ],
-        PlurkImageUrl: [
-            "https://images.plurk.com/5wj6WD0r6y4rLN0DL3sqag.jpg",
-            "https://images.plurk.com/mx_5wj6WD0r6y4rLN0DL3sqag.jpg",
-        ],
-        PlurkPostUrl: [
-            "https://www.plurk.com/p/om6zv4",
-            "https://www.plurk.com/m/p/okxzae",
-        ],
-    }
-
     @classmethod
     def match_url(cls, parsable_url: ParsableUrl) -> PlurkUrl | None:
         instance: PlurkUrl
 
         match parsable_url.url_parts:
+            # https://www.plurk.com/p/om6zv4
+            # https://www.plurk.com/m/p/okxzae
             case *_, "p", post_id:
                 instance = PlurkPostUrl(parsable_url)
                 instance.post_id = post_id
+
+            # https://www.plurk.com/u/ddks2923
+            # https://www.plurk.com/m/u/leiy1225
+            # https://www.plurk.com/s/u/salmonroe13
             case *_, "u", username:
                 instance = PlurkArtistUrl(parsable_url)
                 instance.username = username
+
+            # https://www.plurk.com/m/redeyehare
             case "m", username:
                 instance = PlurkArtistUrl(parsable_url)
                 instance.username = username
+
+            # https://images.plurk.com/5wj6WD0r6y4rLN0DL3sqag.jpg
+            # https://images.plurk.com/mx_5wj6WD0r6y4rLN0DL3sqag.jpg
             case _filename, if parsable_url.subdomain == "images":
                 instance = PlurkImageUrl(parsable_url)
                 instance.image_id = parsable_url.stem.removeprefix("mx_")
+
+            # https://www.plurk.com/redeyehare
+            # https://www.plurk.com/RSSSww/invite/4
             case username, *_:
                 instance = PlurkArtistUrl(parsable_url)
                 instance.username = username
+
             case _:
                 return None
 
--- tests/extractors/plurk_test.py
+++ tests/extractors/plurk_test.py
@@ -1,27 +1,26 @@
-from ward import test
-
-from danboorutools.models.url import Url
+from danboorutools.logical.extractors.plurk import PlurkArtistUrl, PlurkImageUrl, PlurkPostUrl
+from tests.extractors import generate_parsing_suite
 
 urls = {
-    "https://www.plurk.com/m/redeyehare": "https://www.plurk.com/redeyehare",
-    "https://www.plurk.com/u/ddks2923": "https://www.plurk.com/ddks2923",
-    "https://www.plurk.com/m/u/leiy1225": "https://www.plurk.com/leiy1225",
-    "https://www.plurk.com/s/u/salmonroe13": "https://www.plurk.com/salmonroe13",
-    "https://www.plurk.com/redeyehare": "https://www.plurk.com/redeyehare",
-    "https://www.plurk.com/RSSSww/invite/4": "https://www.plurk.com/RSSSww",
+    PlurkArtistUrl: {
+        "https://www.plurk.com/m/redeyehare": "https://www.plurk.com/redeyehare",
+        "https://www.plurk.com/u/ddks2923": "https://www.plurk.com/ddks2923",
+        "https://www.plurk.com/m/u/leiy1225": "https://www.plurk.com/leiy1225",
+        "https://www.plurk.com/s/u/salmonroe13": "https://www.plurk.com/salmonroe13",
+        "https://www.plurk.com/redeyehare": "https://www.plurk.com/redeyehare",
+        "https://www.plurk.com/RSSSww/invite/4": "https://www.plurk.com/RSSSww",
+    },
+    PlurkImageUrl: {
 
-    "https://images.plurk.com/5wj6WD0r6y4rLN0DL3sqag.jpg": "https://images.plurk.com/5wj6WD0r6y4rLN0DL3sqag.jpg",
-    "https://images.plurk.com/mx_5wj6WD0r6y4rLN0DL3sqag.jpg": "https://images.plurk.com/5wj6WD0r6y4rLN0DL3sqag.jpg",
+        "https://images.plurk.com/5wj6WD0r6y4rLN0DL3sqag.jpg": "https://images.plurk.com/5wj6WD0r6y4rLN0DL3sqag.jpg",
+        "https://images.plurk.com/mx_5wj6WD0r6y4rLN0DL3sqag.jpg": "https://images.plurk.com/5wj6WD0r6y4rLN0DL3sqag.jpg",
 
-    "https://www.plurk.com/p/om6zv4": "https://www.plurk.com/p/om6zv4",
-    "https://www.plurk.com/m/p/okxzae": "https://www.plurk.com/p/okxzae",
+    },
+    PlurkPostUrl: {
+        "https://www.plurk.com/p/om6zv4": "https://www.plurk.com/p/om6zv4",
+        "https://www.plurk.com/m/p/okxzae": "https://www.plurk.com/p/okxzae",
+    },
 }
 
 
-for original_string, normalized_string in urls.items():
-    parsed_url = Url.parse(original_string)
-    domain = parsed_url.parsed_url.domain
-
-    @test(f"Normalizing {domain}: {original_string}", tags=["parsing", "normalization", domain])
-    def _(_parsed_url=parsed_url, _normalized_string=normalized_string) -> None:
-        assert _parsed_url.normalized_url == _normalized_string
+generate_parsing_suite(urls)
