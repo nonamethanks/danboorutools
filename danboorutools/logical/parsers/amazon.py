246b786cc2ffec249fed82e384a6a9035b34c99b
nonamethanks <hellafrickingepic@gmail.com>
2023-03-12 12:25:50 +0100

    Exclude amazon wishlist urls

--- danboorutools/logical/parsers/amazon.py
+++ danboorutools/logical/parsers/amazon.py
@@ -2,6 +2,7 @@ from danboorutools.exceptions import UnparsableUrl
 from danboorutools.logical.extractors.enty import EntyImageUrl
 from danboorutools.logical.extractors.skeb import SkebImageUrl
 from danboorutools.logical.parsers import ParsableUrl, UrlParser
+from danboorutools.models.url import UselessUrl
 
 RESERVED_NAMES = {"blogs", "en", "messages", "posts", "products", "ranking",
                   "search", "series", "service_navigations", "signout", "titles", "users"}
@@ -14,7 +15,7 @@ class AmazonawsComParser(UrlParser):
         ],
         SkebImageUrl: [
             "https://skeb-production.s3.ap-northeast-1.amazonaws.com/uploads/outputs/20f9d68f-50ec-44ae-8630-173fc38a2d6a?response-content-disposition=attachment%3B%20filename%3D%22458093-1.output.mp4%22%3B%20filename%2A%3DUTF-8%27%27458093-1.output.mp4&response-content-type=video%2Fmp4&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIVPUTFQBBL7UDSUA%2F20220221%2Fap-northeast-1%2Fs3%2Faws4_request&X-Amz-Date=20220221T200057Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=7f028cfd9a56344cf1d42410063fad3ef30a1e47b83cef047247e0c37df01df0",
-        ]
+        ],
     }
 
     @classmethod
@@ -45,6 +46,20 @@ class AmazonawsComParser(UrlParser):
                 instance.image_uuid = image_uuid
                 instance.page = None
                 instance.post_id = None
+            case _:
+                return None
+
+        return instance
+
+
+class AmazonComParser(UrlParser):
+    domains = ["amazon.com", "amazon.jp"]
+
+    @classmethod
+    def match_url(cls, parsable_url: ParsableUrl) -> UselessUrl | None:
+        match parsable_url.url_parts:
+            case "hz", "wishlist", "ls", _wishlist_id:
+                instance = UselessUrl(parsable_url)
             case _:
                 return None
 
