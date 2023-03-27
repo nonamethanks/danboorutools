ceb0ba8f5932c8be05d21d057562dd5e420b6a12
nonamethanks <hellafrickingepic@gmail.com>
2023-03-27 14:29:00 +0200

    Add fanbox feed extraction

---
+++ danboorutools/logical/feeds/fanbox.py
@@ -0,0 +1,37 @@
+from collections.abc import Iterator
+
+from danboorutools.logical.sessions.fanbox import FanboxSession
+from danboorutools.logical.urls.fanbox import FanboxPostUrl
+from danboorutools.models.feed import Feed
+
+
+class FanboxFeed(Feed):
+    session = FanboxSession()
+
+    def _extract_posts_from_each_page(self) -> Iterator[list[int]]:
+        data_url = "https://api.fanbox.cc/post.listHome?limit=10"
+        while True:
+            page_json = self.session.get_and_parse_fanbox_json(data_url, use_cookies=True)
+
+            if not (data_url := page_json["nextUrl"]):
+                return
+
+            if not (posts_json := page_json["items"]):
+                raise NotImplementedError("No results found. Check cookies.")
+
+            yield [post_json["id"] for post_json in posts_json]
+
+    def _process_post(self, post_object: int) -> None:
+        post_data = self.session.post_data(post_object)
+
+        if not post_data.assets:
+            return
+
+        post = FanboxPostUrl.build(FanboxPostUrl, username=post_data.creatorId, post_id=post_data.id)
+
+        self._register_post(
+            post=post,
+            assets=post_data.assets,
+            score=post_data.likeCount,
+            created_at=post_data.publishedDatetime,
+        )
--- danboorutools/logical/sessions/fanbox.py
+++ danboorutools/logical/sessions/fanbox.py
@@ -1,15 +1,35 @@
 from __future__ import annotations
 
+import os
+from datetime import datetime
+
 from danboorutools.logical.sessions import Session
 from danboorutools.models.url import Url
-from danboorutools.util.misc import BaseModel
+from danboorutools.util.misc import BaseModel, memoize
 
 
 class FanboxSession(Session):
     def artist_data(self, username: str) -> FanboxArtistData:
         headers = {"Origin": f"https://{username}.fanbox.cc", "Referer": f"https://{username}.fanbox.cc/"}
-        response = self.get_json_cached(f"https://api.fanbox.cc/creator.get?creatorId={username}", headers=headers)
-        return FanboxArtistData(**response["body"])
+        artist_data = self.get_and_parse_fanbox_json(f"https://api.fanbox.cc/creator.get?creatorId={username}", headers=headers)
+        return FanboxArtistData(**artist_data)
+
+    @memoize
+    def post_data(self, post_id: int) -> FanboxPostData:
+        post_json = self.get_and_parse_fanbox_json(f"https://api.fanbox.cc/post.info?postId={post_id}")
+        return FanboxPostData(**post_json)
+
+    def get_and_parse_fanbox_json(self, json_url: str, *args, use_cookies: bool = False, **kwargs) -> dict:
+        kwargs["headers"] = {"Origin": "https://www.fanbox.cc"} | kwargs.get("headers", {})
+
+        if use_cookies:
+            kwargs["cookies"] = {"FANBOXSESSID": os.environ["FANBOX_FANBOXSESSID"]} | kwargs.get("cookies", {})
+
+        data = self.get_json_cached(json_url, *args, **kwargs)
+        if data.get("error") == "general_error":
+            raise NotImplementedError(f"Couldn't get the data from {json_url}: {data}")
+
+        return data["body"]
 
 
 class _UserData(BaseModel):
@@ -36,3 +56,34 @@ class FanboxArtistData(BaseModel):
             results += [pixiv_url]
 
         return results
+
+
+class FanboxPostData(BaseModel):
+    id: int
+
+    creatorId: str
+
+    likeCount: int
+    publishedDatetime: datetime
+
+    feeRequired: int
+
+    body: dict | None
+
+    @property
+    def assets(self) -> list[str]:
+        assets: list[str] = []
+        if self.feeRequired:
+            return assets
+
+        assert self.body is not None
+        assets += [i["originalUrl"] for i in self.body.get("images", [])]
+        assets += [v["originalUrl"] for v in self.body.get("imageMap", {}).values()]
+        assets += [v["url"] for v in self.body.get("fileMap", {}).values()]
+
+        if "blocks" in self.body:
+            image_positions = [i["imageId"] for i in self.body["blocks"] if i["type"] == "image"]
+            image_index = {k: v for v, k in enumerate(image_positions)}
+            assets.sort(key=lambda x: image_index.get(x.split("/")[-1].split(".")[0], 0))
+
+        return assets
