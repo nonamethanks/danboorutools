455afcf19e2d0a0b400faacd5611a8cabb419b10
nonamethanks <hellafrickingepic@gmail.com>
2023-04-19 10:34:46 +0200

    Don't scrape carrd urls, it's too hard

--- danboorutools/logical/urls/carrd.py
+++ danboorutools/logical/urls/carrd.py
@@ -1,7 +1,19 @@
-from danboorutools.models.url import InfoUrl
+from danboorutools.models.url import InfoUrl, Url
 
 
 class CarrdUrl(InfoUrl):
     username: str
 
     normalize_template = "https://{username}.carrd.co"
+
+    @property
+    def related(self) -> list[Url]:
+        return []
+
+    @property
+    def primary_names(self) -> list[str]:
+        return []
+
+    @property
+    def secondary_names(self) -> list[str]:
+        return [self.username]
