779b462f938047e47f1ff4680762c2774363596d
nonamethanks <hellafrickingepic@gmail.com>
2023-11-24 13:06:12 +0100

    taittsuu fixup

--- danboorutools/logical/parsers/taittsuu.py
+++ danboorutools/logical/parsers/taittsuu.py
@@ -1,10 +1,11 @@
 from danboorutools.logical.url_parser import ParsableUrl, UrlParser
-from danboorutools.logical.urls.taittsuu import TaittsuuArtistUrl, TaittsuuPostUrl, TaittsuuUrl
+from danboorutools.logical.urls.taittsuu import TaittsuuArtistUrl, TaittsuuPostUrl
+from danboorutools.models.url import Url, UselessUrl
 
 
 class TaittsuuComParser(UrlParser):
     @classmethod
-    def match_url(cls, parsable_url: ParsableUrl) -> TaittsuuUrl | None:
+    def match_url(cls, parsable_url: ParsableUrl) -> Url | None:
         match parsable_url.url_parts:
             case "users", username:
                 return TaittsuuArtistUrl(parsed_url=parsable_url,
@@ -16,5 +17,7 @@ class TaittsuuComParser(UrlParser):
                 return TaittsuuPostUrl(parsed_url=parsable_url,
                                        username=username,
                                        post_id=int(post_id))
+            case "home", :
+                return UselessUrl(parsable_url)
             case _:
                 return None
