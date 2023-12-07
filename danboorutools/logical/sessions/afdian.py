c0db01b31cdfb724bbf44a9ec22bd9909a3d5e22
nonamethanks <hellafrickingepic@gmail.com>
2023-12-07 15:51:19 +0100

    Handle afdian dead urls

--- danboorutools/logical/sessions/afdian.py
+++ danboorutools/logical/sessions/afdian.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from danboorutools.exceptions import DeadUrlError
 from danboorutools.logical.sessions import Session
 from danboorutools.models.url import Url
 from danboorutools.util.misc import BaseModel, extract_urls_from_string
@@ -7,8 +8,14 @@ from danboorutools.util.misc import BaseModel, extract_urls_from_string
 
 class AfdianSession(Session):
     def artist_data(self, username: str) -> AfdianArtistData:
-        response = self.get_json(f"https://afdian.net/api/user/get-profile-by-slug?url_slug={username}")
-        return AfdianArtistData(**response["data"]["user"])
+        url = f"https://afdian.net/api/user/get-profile-by-slug?url_slug={username}"
+        response = self.get_json(url)
+
+        if response.get("em") == "用户不存在":  # User does not exist
+            raise DeadUrlError(status_code=404, original_url=url)
+
+        user_data = response["data"]["user"]
+        return AfdianArtistData(**user_data)
 
 
 class AfdianArtistData(BaseModel):
