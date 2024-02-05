f2f44f4b0a3ed62d62a911e1ebef6bfff6b73ae9
nonamethanks <hellafrickingepic@gmail.com>
2024-02-05 18:30:26 +0100

    Remove unused code

--- danboorutools/models/feed.py
+++ danboorutools/models/feed.py
@@ -4,14 +4,11 @@ import datetime
 import inspect
 from functools import cached_property, lru_cache
 from importlib import import_module
-from typing import TYPE_CHECKING, Generic, TypeVar
+from typing import TypeVar
 
-from danboorutools import logger, settings
+from danboorutools import settings
 from danboorutools.logical.sessions import Session
-from danboorutools.models.has_posts import FoundKnownPost, HasPosts
-
-if TYPE_CHECKING:
-    from collections.abc import Iterator
+from danboorutools.models.has_posts import HasPosts
 
 feeds: list[type[Feed]] = []
 
@@ -63,46 +60,4 @@ class Feed(HasPosts):
         raise NotImplementedError(self)
 
 
-ArtistTypeVar = TypeVar("ArtistTypeVar")
-
 PostDataVar = TypeVar("PostDataVar")
-
-
-class FeedWithSeparateArtists(Feed, Generic[ArtistTypeVar, PostDataVar]):
-    quit_early_page = 1
-
-    def _extract_all_posts(self) -> None:
-        collected_artists = self._extract_artists()
-        for index, artist in enumerate(collected_artists):
-            logger.info(f"\nAt artist {index+1} of {len(collected_artists)}: <e>{artist}</e>.")
-            try:
-                self.__artist_subloop(artist)
-            except FoundKnownPost:
-                logger.info("Reached a previously-seen post. Moving to the next artist...")
-                continue
-
-        self._post_scan_hook()
-
-    def __artist_subloop(self, artist: ArtistTypeVar) -> None:
-        for page, post_objects in enumerate(self._extract_posts_from_each_artist(artist=artist)):
-            logger.info(f"At page {page + 1}...")
-
-            for post_data in post_objects:
-                self._process_post(post_data)
-
-            if not self.known_posts and page + 1 >= self.quit_early_page:
-                logger.info("Stopping early because it's a first-time scan...")
-                return
-
-    def _extract_artists(self) -> list[ArtistTypeVar]:
-        raise NotImplementedError(f"{self} hasn't implemented artist extraction.")
-
-    def _post_scan_hook(self) -> None:
-        return None
-
-    def _extract_posts_from_each_artist(self, artist: ArtistTypeVar) -> Iterator[list[PostDataVar]]:
-        raise NotImplementedError(f"{self} hasn't implemented artist processing.")
-
-    def _extract_posts_from_each_page(self) -> Iterator[list[PostDataVar]]:
-        while True:
-            raise NotImplementedError("Unused")
