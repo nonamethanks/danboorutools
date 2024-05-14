fc0427f40837b622f927b7691d50e600483fc411
nonamethanks <hellafrickingepic@gmail.com>
2024-05-14 14:55:59 +0200

    Switch to chrome headless new mode

--- danboorutools/logical/browser.py
+++ danboorutools/logical/browser.py
@@ -30,7 +30,7 @@ class Browser(Chrome):
 
         options = Options()
 
-        options.add_argument("--headless")
+        options.add_argument("--headless=new")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--ignore-certificate-errors")
