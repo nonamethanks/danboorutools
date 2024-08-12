1e93bc499a294db61d839a035dcbf95bd812b774
nonamethanks <hellafrickingepic@gmail.com>
2024-08-12 14:02:54 +0200

    Better promotion suggestion interface

--- danboorutools/scripts/promotion_suggestions.py
+++ danboorutools/scripts/promotion_suggestions.py
@@ -1,8 +1,12 @@
 from __future__ import annotations
 
+import textwrap
 from dataclasses import dataclass
 from datetime import UTC, datetime, timedelta
 from typing import TYPE_CHECKING
+from urllib.parse import quote_plus
+
+import click
 
 from danboorutools import logger
 from danboorutools.logical.sessions.danbooru import danbooru_api, kwargs_to_include
@@ -26,7 +30,9 @@ CONTRIB_MAX_DEL_PERC = 5
 BUILDER_MAX_DEL_PERC = 15
 
 
-def main() -> None:
+@click.command()
+@click.option("--manual", "-m", is_flag=True, show_default=True, default=False)
+def main(manual: bool = False) -> None:
     logger.info("Gathering data...")
 
     recent_uploaders = get_recent_uploaders()
@@ -49,17 +55,27 @@ def main() -> None:
             # user does not have enough uploads or edits to be considered
             pass
 
-    logger.info("Contributor candidates:")
+    manual_cycle = []
+
+    if not manual:
+        logger.info("Contributor candidates:")
     for candidate in candidates:
         if candidate.for_contributor:
-            logger.info(candidate.self_string)
+            if manual:
+                manual_cycle.append(candidate)
+            else:
+                logger.info(candidate.self_string)
 
-    logger.info("")
-    logger.info("Builder/Contributor candidates:")
+    if not manual:
+        logger.info("")
+        logger.info("Builder/Contributor candidates:")
 
     for candidate in candidates:
         if candidate.for_builder:
-            logger.info(candidate.self_string)
+            if manual:
+                manual_cycle.append(candidate)
+            else:
+                logger.info(candidate.self_string)
 
     logger.info("")
     remaining: list[Candidate] = []
@@ -75,10 +91,44 @@ def main() -> None:
                 merge_candidate(candidate, match)
                 remaining.append(candidate)
 
-    if remaining:
+    if remaining and not manual:
         logger.info("Builder candidates:")
         for candidate in [c for c in remaining if c.level != 32 and c.active]:
             logger.info(candidate.self_string)
+
+    if manual:
+        manual_loop(*manual_cycle, *[c for c in remaining if c.level != 32 and c.active])
+
+
+def manual_loop(*candidates: Candidate) -> None:
+    index = 0
+    while True:
+        candidate = candidates[index]
+        logger.info(f"Candidate {index + 1}:")
+        logger.info(candidate.self_presentation)
+        logger.info(f"{len(candidates) - index - 2} candidates left.")
+
+        while True:
+            logger.info("[N]ext / [P]rev")
+            if (_input := input("").strip().lower()) in ["", "n"]:
+                index += 1
+                if index >= len(candidates):
+                    logger.info("No more candidates.")
+                    return
+                else:
+                    break
+            elif _input in ["p"]:
+                index -= 1
+                if index < 0:
+                    logger.error("No previous candidates.")
+                    index = 0
+                else:
+                    break
+            elif _input in ["r"]:
+                candidate.refresh()
+                break
+            else:
+                logger.error("Invalid output.")
 
 
 def merge_candidate(candidate: Candidate, user: DanbooruUser) -> None:
@@ -118,7 +168,7 @@ def get_recent_uploaders() -> list[Candidate]:
         recent_uploaders.append(Candidate(
             name=name.replace(" ", "_"),
             recent_uploads=uploader_data["posts"],
-            recent_deleted=deleted_map.get(name)),
+            recent_deleted=deleted_map.get(name, 0)),
         )
 
     return recent_uploaders
@@ -175,7 +225,7 @@ class Candidate:
     def active(self) -> bool:
         if self.is_deleted:   # can also be None
             return False
-        if self.is_banned:  # can also be None
+        if self.is_banned:  # can also be None  # noqa: SIM103
             return False
         return True
 
@@ -190,6 +240,10 @@ class Candidate:
     @property
     def url(self) -> str:
         return f"https://danbooru.donmai.us/users/{self.id}"
+
+    @property
+    def deleted_url(self) -> str:
+        return f"https://danbooru.donmai.us/posts?tags=user:{quote_plus(self.name)}+status:deleted+age:%3C2mo"
 
     @property
     def self_string(self) -> str:
@@ -210,8 +264,68 @@ class Candidate:
         else:
             string += f"{self.total_uploads:>6} U | "
 
-        if not self.delete_ratio:
+        string += f"{self.recent_uploads if self.recent_uploads is not None else "???":>6} / {self.recent_deleted_colored} = {self.delete_ratio_colored} | "  # noqa: E501
+
+        string += f" {self.url}"
+
+        return string
+
+    def refresh(self) -> None:
+        logger.info("Refreshing data...")
+        user, = danbooru_api.users(id=self.id)
+        merge_candidate(self, user)
+        date_tag = f"date:{START_DATE.strftime("%Y-%m-%d")}..{END_DATE.strftime("%Y-%m-%d")}"
+        tags = [f"user:{self.name}", date_tag]
+        self.recent_uploads = danbooru_api.post_counts(tags=tags)
+        self.recent_deleted = danbooru_api.post_counts(tags=[*tags, "status:deleted"])
+        logger.info("Refreshed.")
+
+    @property
+    def self_presentation(self) -> str:
+        potential_level = "Contributor" if self.for_contributor else "Builder"
+        potential_color = "bg #ffb671" if self.for_contributor else "bg #702963"
+
+        ruc = "GREEN" if self.delete_ratio < BUILDER_MAX_DEL_PERC else "RED"
+        tuc = "GREEN" if self.total_uploads > MIN_STANDALONE_UPLOADS else "YELLOW"
+
+        return textwrap.dedent(f"""
+
+            <{self.level_color}> User #{self.id}, {self.name}, {self.level_string} </> - candidate for <{potential_color}> {potential_level} </>
+
+            Url: <c>{self.url}</c>
+
+            Recent Deleted: <c>{self.deleted_url}</c>
+
+            Total Uploads: <{tuc}> {self.total_uploads:_} </>. Recent uploads: <{ruc}> {self.recent_uploads:_} </>. Deleted: {self.recent_deleted_colored} ({self.delete_ratio_colored})
+
+            Total Edits: {self.total_edits:_}.
+
+        """)  # noqa: E501
+
+    @property
+    def recent_deleted_colored(self) -> str:
+        if self.recent_deleted == 0:
+            delete_color = "WHITE"
+        elif self.recent_deleted is None:
+            delete_color = "MAGENTA"
+        elif self.recent_deleted > CONTRIB_RISKY_DEL_COUNT:
+            delete_color = "RED" if self.recent_deleted > CONTRIB_MAX_DEL_COUNT else "YELLOW"
+        else:
+            delete_color = "GREEN"
+
+        return f"<{delete_color}>{self.recent_deleted if self.recent_deleted is not None else "???":^5}</{delete_color}>"
+
+    @property
+    def delete_ratio(self) -> int:
+        assert self.recent_deleted is not None
+        return (self.recent_deleted / self.recent_uploads) * 100
+
+    @property
+    def delete_ratio_colored(self) -> str:
+        if self.recent_deleted == 0:
             ratio_color = "WHITE"
+        elif self.recent_deleted is None:
+            ratio_color = "MAGENTA"
         elif self.delete_ratio > BUILDER_MAX_DEL_PERC:
             ratio_color = "RED"
         elif CONTRIB_MAX_DEL_PERC < self.delete_ratio < BUILDER_MAX_DEL_PERC:
@@ -219,30 +333,7 @@ class Candidate:
         else:
             ratio_color = "GREEN"
 
-        if not self.recent_deleted:
-            delete_color = "WHITE"
-        elif self.recent_deleted > CONTRIB_RISKY_DEL_COUNT:
-            delete_color = "RED" if self.recent_deleted > CONTRIB_MAX_DEL_COUNT else "YELLOW"
-        else:
-            delete_color = "GREEN"
-
-        if not (self.recent_deleted is None and self.recent_uploads is None):
-            delete_string = f"<{delete_color}>{self.recent_deleted if self.recent_deleted is not None else "???":^5}</{delete_color}>"
-            if self.delete_ratio is not None:
-                ratio_string = f"<{ratio_color}>{self.delete_ratio:>6.2f} </{ratio_color}> %"
-            else:
-                ratio_string = f"<{ratio_color}>{"???":>6} </{ratio_color}> %"
-            string += f"{self.recent_uploads if self.recent_uploads is not None else "???":>6} / {delete_string} = {ratio_string} | "
-
-        string += f" {self.url}"
-
-        return string
-
-    @property
-    def delete_ratio(self) -> int:
-        if self.recent_deleted is None:
-            return None
-        return (self.recent_deleted / self.recent_uploads) * 100
+        return f"<{ratio_color}>{f"{self.delete_ratio:>6.2f}" if self.recent_deleted is not None else "  ??? "}% </{ratio_color}>"
 
     @property
     def level_color(self) -> str:
