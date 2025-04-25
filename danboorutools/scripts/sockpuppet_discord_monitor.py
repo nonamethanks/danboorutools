580f67930fcf8433b58a5005283d240c25e2c7f0
nonamethanks <hellafrickingepic@gmail.com>
2025-04-25 13:00:31 +0200

    Refactor sock monitor to allow for multiple socks

--- danboorutools/scripts/sockpuppet_discord_monitor.py
+++ danboorutools/scripts/sockpuppet_discord_monitor.py
@@ -4,6 +4,7 @@ import datetime
 import os
 import re
 import time
+from dataclasses import dataclass
 from datetime import UTC
 from pathlib import Path
 from typing import Literal
@@ -11,6 +12,7 @@ from typing import Literal
 import click
 import yaml
 from discord_webhook import DiscordEmbed, DiscordWebhook
+from pydantic import Field
 
 from danboorutools import logger, settings
 from danboorutools.logical.progress_tracker import ProgressTracker
@@ -18,14 +20,77 @@ from danboorutools.logical.sessions.danbooru import DanbooruApi, danbooru_api
 from danboorutools.models.danbooru import DanbooruUser, DanbooruUserEvent
 from danboorutools.util.misc import BaseModel
 
-SOCK_AUTOBAN_MESSAGE = os.environ["DANBOORUTOOLS_SOCKPUPPET_AUTORENAME_MESSAGE"].strip()
-if SOCK_AUTOBAN_MESSAGE and "sockpuppet of user #" not in SOCK_AUTOBAN_MESSAGE.lower():
-    raise NotImplementedError("Message must be like 'Sockpuppet of user #'")
-SOCK_AUTOBAN_CARRIER = os.environ["DANBOORUTOOLS_SOCKPUPPET_AUTOBAN_CARRIER"].strip()
-SOCK_AUTOBAN_IP_PREFIXES = os.environ["DANBOORUTOOLS_SOCKPUPPET_AUTOBAN_IP_PREFIX"].strip().split(",")
+
+class BanEvader(BaseModel):
+    name: str
+    ban_message: str = Field(min_length=3)
+    carrier: str | None = None
+    ip_prefixes: list[str]
+    name_patterns: list[str]
+    rename_socks: bool = False
+    ban_proxies: bool = False
+
+    @property
+    def compiled_name_patterns(self) -> list[re.Pattern]:
+        return [re.compile(p, re.IGNORECASE) for p in self.name_patterns]
+
+
+    def signup_is_sock(self, signup: DanbooruUserEvent) -> bool:
+        logger.info(f"Checking user {signup.user} against pattern for ban evader {self.name}...")
+
+        if not any(pattern.search(signup.user.name) for pattern in self.compiled_name_patterns):
+            logger.info(f"User {signup.user} does not match name regex. Aborting.")
+            return False
+        logger.info(f"User {signup.user} matches name regex")
+
+        if not self.ip_prefixes and not self.carrier and not self.ban_proxies:
+            raise ValueError("No IP prefixes, carrier or proxy ban configured. Please check your sock_config.yaml file.")
+
+        if self.ip_prefixes and not self.check_ip_prefixes(signup):
+            return False
+
+        if self.carrier and not self.check_sock_carrier(signup):
+            return False
+
+        if self.ban_proxies:
+            return self.check_proxies(signup)
+
+        return True
+
+    def check_ip_prefixes(self, signup: DanbooruUserEvent) -> bool:
+        last_ip_addr: str = signup.user._raw_data["last_ip_addr"]
+        if self.ip_prefixes:
+            if not last_ip_addr.startswith(self.ip_prefixes):
+                logger.info(f"User {signup.user} does not match IP prefixes. Aborting.")
+                return False
+            logger.info(f"User {signup.user} matches IP prefixes {self.ip_prefixes}")
+
+        return True
+
+    def check_sock_carrier(self, signup: DanbooruUserEvent) -> bool:
+        last_ip_addr: str = signup.user._raw_data["last_ip_addr"]
+        ip_addr_data = danbooru_api.danbooru_request("GET", f"ip_addresses/{last_ip_addr}.json")
+        if not ip_addr_data["carrier"]:
+            return False
+        if ip_addr_data["carrier"].lower() != self.carrier.lower():
+            logger.info(f"User {signup.user} does not have carrier {self.carrier}. Aborting.")
+            return False
+        logger.info(f"User {signup.user} matches carrier {self.carrier}")
+        return True
+
+    def check_proxies(self, signup: DanbooruUserEvent) -> bool:
+        last_ip_addr: str = signup.user._raw_data["last_ip_addr"]
+        ip_addr_data = danbooru_api.danbooru_request("GET", f"ip_addresses/{last_ip_addr}.json")
+        if ip_addr_data["is_proxy"]:
+            logger.info(f"User {signup.user} is using a proxy.")
+            return True
+        logger.info(f"User {signup.user} is not using a proxy. Aborting.")
+        return False
+
+
 
 sock_config = yaml.safe_load(Path(settings.BASE_FOLDER / "sock_config.yaml").read_text(encoding="utf-8"))
-SOCK_PATTERNS = [re.compile(p, re.I) for p in sock_config["patterns"]]
+ban_evaders = [BanEvader(**evader) for evader in sock_config["socks"]]
 
 
 class SockpuppetDetector:
@@ -40,20 +105,23 @@ class SockpuppetDetector:
         else:
             raise ValueError(self.mode)
 
+        self.enable_autobans = os.environ.get("SOCKPUPPET_CHECK_ENABLE_AUTOBANS", "false").lower() in ["true", "1", "yes"]
+
         self.last_checked_session = ProgressTracker("SOCKPUPPET_DETECTOR_LAST_CHECKED_SESSION", 0)
         self.old_hooks = ProgressTracker[list[dict]]("SOCKPUPPET_DETECTOR_POSTED_HOOKS", [])
 
         self.max_webhook_backchecking = 20
 
-        if SOCK_AUTOBAN_MESSAGE:
-            logger.info(f"<r>Will autoban any user whose previous ban reason started with '{SOCK_AUTOBAN_MESSAGE}'</r>")
-        if SOCK_AUTOBAN_CARRIER and SOCK_AUTOBAN_IP_PREFIXES:
-            logger.info(f"<r>Will autoban any user with certain names and carrier == '{SOCK_AUTOBAN_CARRIER}' "
-                        f"and IP starting with {SOCK_AUTOBAN_IP_PREFIXES}</r>")
-
-            logger.info("<r>Configured autoban patterns:</r>")
-            for pattern in SOCK_PATTERNS:
-                logger.info(f">  <r>{pattern}</r>")
+        if ban_evaders:
+            logger.info("<r>The following autobans are configured:</r>")
+            for evader in ban_evaders:
+                logger.info(f"  <r>{evader.name} ({evader.ban_message})</r>")
+                logger.info(f"  <r>Carrier: {evader.carrier}</r>")
+                logger.info(f"  <r>IP prefixes: {', '.join(evader.ip_prefixes)}</r>")
+                logger.info( "    <r>Patterns:</r>")
+                for pattern in evader.compiled_name_patterns:
+                    logger.info(f"      <r>{pattern}</r>")
+                logger.info("")
 
     def detect_and_post(self) -> None:
         latest_signups = self.get_latest_signups()
@@ -109,18 +177,20 @@ class SockpuppetDetector:
             )
 
             if not (other_events := [e for e in events if e.user.name != signup.user.name]):
-                if SOCK_AUTOBAN_CARRIER and SOCK_AUTOBAN_IP_PREFIXES:
-                    self._check_for_sock(signup, [])
+                for ban_evader in ban_evaders:
+                    self._check_for_sock(signup=signup, other_users=[], ban_evader=ban_evader)
                 continue
 
             other_users_map = {event.user.id: event.user for event in other_events}
             other_users = [name for _id, name in sorted(other_users_map.items(), key=lambda x: x[0])]
 
-            if SOCK_AUTOBAN_CARRIER and SOCK_AUTOBAN_IP_PREFIXES and self._check_for_sock(signup, other_users):
-                for signup in signups:  # noqa: PLW2901 # be silent machine, I know what I'm doing
-                    if signup.user.id in [user.id for user in other_users]:
-                        signup.user.is_banned = True
-                continue
+            # have to do it all over again for signups from multiple accounts
+            for ban_evader in ban_evaders:
+                if self._check_for_sock(signup=signup, other_users=other_users, ban_evader=ban_evader):
+                    for signup in signups:  # noqa: PLW2901 # be silent machine, I know what I'm doing
+                        if signup.user.id in [user.id for user in other_users]:
+                            signup.user.is_banned = True
+                    continue
 
             previous_ban_reasons: list[str] = [ban["reason"] for user in other_users for ban in user._raw_data["bans"]]
             if previous_ban_reasons:
@@ -128,14 +198,11 @@ class SockpuppetDetector:
                 if all(r.lower().strip().strip(".") in shared_account_reasons for r in previous_ban_reasons):
                     continue
 
-                if SOCK_AUTOBAN_MESSAGE and any(ban_r.lower().startswith(SOCK_AUTOBAN_MESSAGE.lower()) for ban_r in previous_ban_reasons):
-                    user_to_ban = signup.user
-                    if not user_to_ban.is_banned:
-                        self.ban_user(user_to_ban, validate=True)
-
-                    if user_to_ban.name != str(user_to_ban.id):
-                        logger.info(f"<r>RENAMING USER {user_to_ban} {user_to_ban.name} -> '{user_to_ban.id}' </r>")
-                        danbooru_api.rename_user(user_to_ban.id, new_name=user_to_ban.id)
+                for ban_evader in ban_evaders:
+                    if any(ban_r.lower().startswith(ban_evader.ban_message.lower()) for ban_r in previous_ban_reasons):
+                        user_to_ban = signup.user
+                        if not user_to_ban.is_banned:
+                            self.ban_user(user_to_ban, ban_evader=ban_evader, validate=True)
 
             found.append({
                 "sock": signup.user,
@@ -145,15 +212,24 @@ class SockpuppetDetector:
 
         return found
 
-    def ban_user(self, user_to_ban: DanbooruUser, validate: bool = True) -> None:
+    def ban_user(self, user_to_ban: DanbooruUser, ban_evader: BanEvader, validate: bool = True) -> None:
         if validate:
             assert user_to_ban.level <= 20
             assert user_to_ban.created_at
             assert user_to_ban.created_at > (datetime.datetime.now(tz=UTC) - datetime.timedelta(hours=1)), \
                 f"Couldn't ban user {user_to_ban}"
 
-        logger.info(f"<r>BANNING USER {user_to_ban}</r>")
-        danbooru_api.ban_user(user_to_ban.id, reason=SOCK_AUTOBAN_MESSAGE)
+        logger.info(f"<r>BANNING USER {user_to_ban} (sockpuppet of {ban_evader.name})</r>")
+        if self.enable_autobans:
+            danbooru_api.ban_user(user_to_ban.id, reason=ban_evader.ban_message)
+        else:
+            logger.info(f"Skipping autoban for {user_to_ban} because autobans are disabled.")
+        if ban_evader.rename_socks and user_to_ban.name != str(user_to_ban.id):
+            logger.info(f"<r>RENAMING USER {user_to_ban} {user_to_ban.name} -> '{user_to_ban.id}'</r>")
+            if self.enable_autobans:
+                danbooru_api.rename_user(user_to_ban.id, new_name=user_to_ban.id)
+            else:
+                logger.info(f"Skipping rename for {user_to_ban} because autobans are disabled.")
 
     def send_to_discord(self, sock: DanbooruUser, session_id: str,  other_users: list[DanbooruUser]) -> dict:
         webhook = DiscordWebhook(
@@ -221,40 +297,20 @@ class SockpuppetDetector:
 
         logger.info(f"Updated {changed_count} messages.")
 
-    def _check_for_sock(self, signup: DanbooruUserEvent, other_users: list[DanbooruUser]) -> bool:
-        if not any(pattern.search(signup.user.name) for pattern in SOCK_PATTERNS):
+    def _check_for_sock(self, signup: DanbooruUserEvent, other_users: list[DanbooruUser], ban_evader: BanEvader) -> bool:
+        if not ban_evader.signup_is_sock(signup):
             return False
-
-        logger.info(f"User {signup.user} matches name regex")
-
-        last_ip_addr: str = signup.user._raw_data["last_ip_addr"]
-        if not last_ip_addr.startswith(tuple(SOCK_AUTOBAN_IP_PREFIXES)):
-            return False
-
-        logger.info(f"User {signup.user} matches ip prefix {SOCK_AUTOBAN_IP_PREFIXES}.")
-
-        ip_addr_data = danbooru_api.danbooru_request("GET", f"ip_addresses/{last_ip_addr}.json")
-        if ip_addr_data["carrier"].lower() != SOCK_AUTOBAN_CARRIER.lower():
-            return False
-
-        logger.info(f"User {signup.user} matches carrier {SOCK_AUTOBAN_CARRIER}")
 
         user_to_ban = signup.user
         if len([u for u in other_users if not u.is_banned]) > 20:
             raise NotImplementedError("Something's wrong, too many unbanned users.")
 
-        self.ban_user(user_to_ban)
-        if user_to_ban.name != str(user_to_ban.id):
-            logger.info(f"<r>RENAMING USER {user_to_ban} {user_to_ban.name} -> '{user_to_ban.id}' </r>")
-            danbooru_api.rename_user(user_to_ban.id, new_name=user_to_ban.id)
+        self.ban_user(user_to_ban, ban_evader=ban_evader)
 
         for other_user in other_users:
             assert other_user.level <= 20
             if not other_user.is_banned:
-                self.ban_user(other_user, validate=False)
-            if other_user.name != str(other_user.id):
-                logger.info(f"<r>RENAMING USER {other_user} {other_user.name} -> '{other_user.id}' </r>")
-                danbooru_api.rename_user(other_user.id, new_name=other_user.id)
+                self.ban_user(other_user, ban_evader=ban_evader, validate=False)
 
         return True
 
