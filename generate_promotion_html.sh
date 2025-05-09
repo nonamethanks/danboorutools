940958c9f39f237d7b98c869f5161ae818d6d77c
nonamethanks <hellafrickingepic@gmail.com>
2025-05-09 17:20:51 +0200

    Make promotion script more reliable

--- generate_promotion_html.sh
+++ generate_promotion_html.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 WEEK_NUMBER=$(date +%Y-week-%U)
 SHARE_FOLDER="/disks/disk1/share/"
+DANBOORUTOOLS_FOLDER="/home/nonamethanks/danboorutools"
 
 mkdir -p "$SHARE_FOLDER/promotion_archives/"
-poetry run promotions && cp promotions.html "$SHARE_FOLDER/promotions.html" && cp promotions.html "$SHARE_FOLDER/promotion_archives/promotions-$WEEK_NUMBER.html"
+cd "$DANBOORUTOOLS_FOLDER" && poetry run promotions && cp promotions.html "$SHARE_FOLDER/promotions.html" && cp promotions.html "$SHARE_FOLDER/promotion_archives/promotions-$WEEK_NUMBER.html"
